c08e6defd2abd07f0de8f498e5a0fd24ccca2586
oceandie <diego.bruciaferri@metoffice.gov.uk>
2023-08-08 09:07:06 +0000

    more general structure

--- SEAMOUNT/run_exp/run_exp_seamount.sh
+++ SEAMOUNT/run_exp/run_exp_seamount.sh
@@ -26,16 +26,26 @@ isloaded=`module -t list 2> >(grep scitools/default-current)`
 #-------------------------------------------------------------------------------------
 # SETTING the general experimental setup in the namelist
 
-f90nml -g namusr_def -v nn_ini_cond=${exp_cfg} -p ${nam_tmp} "namcfg.tmpA"
-
 case ${exp_cfg} in
   0) # Shchepetkin and McWilliams (2002)
-    # 1. EOS
-    f90nml -g nameos -v ln_seos=.true. -p "namcfg.tmpA" "namcfg.cfg"
+    # 1. INITIAL CONDITION
+    f90nml -g namusr_def \
+           -v nn_ini_cond=${exp_cfg} \
+           -p ${nam_tmp} "namcfg.tmpA"
+    # 2. EOS
+    f90nml -g nameos \
+           -v ln_seos=.true. \
+           -p "namcfg.tmpA" "namcfg.cfg"    
     ;;
   1) # Ezer, Arango and Shchepetkin (2002)
-    # 1. EOS
-    f90nml -g nameos -v ln_eos80=.true. -p "namcfg.tmpA" "namcfg.cfg"
+    # 1. INITIAL CONDITION
+    f90nml -g namusr_def \
+           -v nn_ini_cond=${exp_cfg} \
+           -p ${nam_tmp} "namcfg.tmpA"
+    # 2. EOS
+    f90nml -g nameos \
+           -v ln_eos80=.true. \
+           -p "namcfg.tmpA" "namcfg.cfg"
     ;; 
   2) # Amy Young
     # 0.  Forcing model output with freq of 3h since xios on-line averaging requires that 
@@ -43,38 +53,88 @@ case ${exp_cfg} in
     if [ "${outfreq}" != "3h" ] && [ "${outfreq}" != "1d" ]; then 
        outfreq="3h"
     fi
-    # 1.  Model domain and mesh
-    f90nml -g namusr_def -v rn_xdim=380.0 -v rn_ydim=288.0 -v rn_dx=4000.0 -v rn_dz=450. -p "namcfg.tmpA" "namcfg.tmpB"
-    # 2.  Initial condition
-    f90nml -g namusr_def -v rn_initrho=0.1 -v rn_s=2.0 -p "namcfg.tmpB" "namcfg.tmpC"
-    # 3.  Baroclinic timestep
-    f90nml -g namdom -v rn_Dt=432. -p "namcfg.tmpC" "namcfg.tmpD"
-    # 4.  EOS
-    f90nml -g nameos -v ln_eeos=.true. -p "namcfg.tmpD" "namcfg.tmpE"
-    # 5.  Momentum advection scheme
-    f90nml -g namdyn_adv -v ln_dynadv_vec=.false. -v ln_dynadv_cen2=.true. -p "namcfg.tmpE" "namcfg.tmpF"
-    # 6.  Vorticity / Coriolis scheme
-    f90nml -g namdyn_vor -v ln_dynvor_een=.false. -v ln_dynvor_ene=.true. -p "namcfg.tmpF" "namcfg.tmpG"
-    # 7.  Barotropic timestep
-    f90nml -g namdyn_spg -v nn_e=36 -p "namcfg.tmpG" "namcfg.tmpH"
-    # 8.  Lateral viscosity
-    f90nml -g namdyn_ldf -v rn_Uv=2.0 -p "namcfg.tmpH" "namcfg.tmpI"
-    # 9.  Vertical diff/visc
-    f90nml -g namzdf -v rn_avm0=0.0 -v rn_avt0=0.0 -p "namcfg.tmpI" "namcfg.tmpJ"
-    # 10. MPI halos
-    f90nml -g nammpp -v nn_hls=2 -p "namcfg.tmpJ" "namcfg.cfg"
+    # 1. INITIAL CONDITION
+    f90nml -g namusr_def \
+           -v nn_ini_cond=${exp_cfg} \
+           -p ${nam_tmp} "namcfg.tmpA"
+    # 2.  Model domain and mesh
+    f90nml -g namusr_def \
+           -v rn_xdim=380.0 \
+           -v rn_ydim=288.0 \
+           -v rn_dx=4000.0 \
+           -v rn_dz=450. \
+           -p "namcfg.tmpA" "namcfg.tmpB"
+    # 3.  Initial condition
+    f90nml -g namusr_def \
+           -v rn_initrho=0.1 \
+           -v rn_s=2.0 \
+           -p "namcfg.tmpB" "namcfg.tmpC"
+    # 4.  Baroclinic timestep
+    f90nml -g namdom \
+           -v rn_Dt=432. \
+           -p "namcfg.tmpC" "namcfg.tmpD"
+    # 5.  EOS
+    f90nml -g nameos \
+           -v ln_eeos=.true. \
+           -p "namcfg.tmpD" "namcfg.tmpE"
+    # 6.  Momentum advection scheme
+    f90nml -g namdyn_adv \
+           -v ln_dynadv_vec=.false. \
+           -v ln_dynadv_cen2=.true. \
+           -p "namcfg.tmpE" "namcfg.tmpF"
+    # 7.  Vorticity / Coriolis scheme
+    f90nml -g namdyn_vor \
+           -v ln_dynvor_een=.false. \
+           -v ln_dynvor_ene=.true. \
+           -p "namcfg.tmpF" "namcfg.tmpG"
+    # 8.  Barotropic timestep
+    f90nml -g namdyn_spg \
+           -v nn_e=36 \
+           -p "namcfg.tmpG" "namcfg.tmpH"
+    # 9.  Lateral viscosity
+    f90nml -g namdyn_ldf \
+           -v rn_Uv=2.0 \
+           -p "namcfg.tmpH" "namcfg.tmpI"
+    # 10.  Vertical diff/visc
+    f90nml -g namzdf \
+           -v rn_avm0=0.0 \
+           -v rn_avt0=0.0 \
+           -p "namcfg.tmpI" "namcfg.tmpJ"
+    # 11. MPI halos
+    f90nml -g nammpp \
+           -v nn_hls=2 \
+           -p "namcfg.tmpJ" "namcfg.cfg"
     ;;
   3) # Domain: Ezer et al 2002, Initialisation: Amy Young
     # 1.  Initial condition
-    f90nml -g namusr_def -v rn_initrho=0.1 -v rn_s=2.0 -p "namcfg.tmpA" "namcfg.tmpB"
+    f90nml -g namusr_def \
+           -v nn_ini_cond=2 \
+           -p ${nam_tmp} "namcfg.tmpA"
+    f90nml -g namusr_def \
+           -v rn_initrho=0.1 \
+           -v rn_s=2.0 \
+           -p "namcfg.tmpA" "namcfg.tmpB"
     # 2.  EOS
-    f90nml -g nameos -v ln_eeos=.true. -p "namcfg.tmpB" "namcfg.cfg"   
+    f90nml -g nameos \
+           -v ln_eeos=.true. \
+           -p "namcfg.tmpB" "namcfg.cfg"   
     ;;
   4) # Domain: Amy Young, Initialisation: Ezer et al 2002
-    # 1.  Model domain and mesh
-    f90nml -g namusr_def -v rn_xdim=380.0 -v rn_ydim=288.0 -v rn_dx=4000.0 -v rn_dz=450. -p "namcfg.tmpA" "namcfg.tmpB"
-    # 2. EOS
-    f90nml -g nameos -v ln_eos80=.true. -p "namcfg.tmpB" "namcfg.cfg"
+    # 1. INITIAL CONDITION
+    f90nml -g namusr_def \
+           -v nn_ini_cond=1 \
+           -p ${nam_tmp} "namcfg.tmpA"
+    # 2.  Model domain and mesh
+    f90nml -g namusr_def \
+           -v rn_xdim=380.0 \
+           -v rn_ydim=288.0 \
+           -v rn_dx=4000.0 \
+           -v rn_dz=450. \
+           -p "namcfg.tmpA" "namcfg.tmpB"
+    # 3. EOS
+    f90nml -g nameos \
+           -v ln_eos80=.true. \
+           -p "namcfg.tmpB" "namcfg.cfg"
     ;;
 esac
 
@@ -83,10 +143,9 @@ nam_tmp="${basedir}/namcfg.cfg"
 
 #-------------------------------------------------------------------------------------
 #for vco in sig s94 vqs zco; do
-#for vco in sig s94 vqs; do
 vco=sig
 
-    for hpg in sco prj djc djcr ffl ffq_cub ffq_ccs fflr; do
+    for hpg in sco prj djc djcr ffl ffq_cub ffq_ccs fflr ffq_cubr ffq_ccsr; do
 
         for ini in pnt ave; do
 
@@ -107,7 +166,9 @@ vco=sig
                    # UPDATING NAMELIST -----------
 
                    # 0. Experience  name
-                   f90nml -g namrun -v cn_exp=${expname} -p ${nam_tmp} ${nam_cfg}".tmp0"
+                   f90nml -g namrun \
+                          -v cn_exp=${expname} \
+                          -p ${nam_tmp} ${nam_cfg}".tmp0"
 
                    # 1. Run length
                    if [ "${outfreq}" == "1ts" ]; then
@@ -121,68 +182,132 @@ vco=sig
                       fi
                       nn_itend=$((secxday/rn_rDT*expdays))
                    fi
-                   f90nml -g namrun -v nn_itend=${nn_itend} -p ${nam_cfg}".tmp0" ${nam_cfg}".tmp1"  
+                   f90nml -g namrun \
+                          -v nn_itend=${nn_itend} \
+                          -p ${nam_cfg}".tmp0" ${nam_cfg}".tmp1"  
 
                    # 2. Vertical coordinates
                    case ${vco} in
                      sig)
-                       f90nml -g namusr_def -v ln_sco=.true. -p ${nam_cfg}".tmp1" ${nam_cfg}".tmp2"
+                       f90nml -g namusr_def \
+                              -v ln_sco=.true. \
+                              -p ${nam_cfg}".tmp1" ${nam_cfg}".tmp2"
                        ;;
                      s94)
-                       f90nml -g namusr_def -v ln_sco=.true. -v ln_s_sh94=.true. -p ${nam_cfg}".tmp1" ${nam_cfg}".tmp2"
+                       f90nml -g namusr_def \
+                              -v ln_sco=.true. \
+                              -v ln_s_sh94=.true. \
+                              -p ${nam_cfg}".tmp1" ${nam_cfg}".tmp2"
                        ;;
                      vqs)
-                       f90nml -g namusr_def -v ln_sco=.true. -v ln_s_sh94=.true. -v ln_vqs=.true. -v rn_rmax=0.2 -p ${nam_cfg}".tmp1" ${nam_cfg}".tmp2"
+                       f90nml -g namusr_def \
+                              -v ln_sco=.true. \
+                              -v ln_s_sh94=.true. \
+                              -v ln_vqs=.true. \
+                              -v rn_rmax=0.2 \
+                              -p ${nam_cfg}".tmp1" ${nam_cfg}".tmp2"
                        ;;
                      zco)
-                       f90nml -g namusr_def -v ln_sco=.true. -v ln_s_sh94=.true. -v ln_vqs=.true. -v rn_rmax=0.0 -p ${nam_cfg}".tmp1" ${nam_cfg}".tmp2"
+                       f90nml -g namusr_def \
+                              -v ln_sco=.true. \
+                              -v ln_s_sh94=.true. \
+                              -v ln_vqs=.true. \
+                              -v rn_rmax=0.0 \
+                              -p ${nam_cfg}".tmp1" ${nam_cfg}".tmp2"
                    esac
 
                    # 3. HPG scheme
                    case ${hpg} in
                      sco|prj|djc)
-                       f90nml -g namdyn_hpg -v ln_hpg_${hpg}=.true. -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
+                       f90nml -g namdyn_hpg \
+                              -v ln_hpg_${hpg}=.true. \
+                              -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
                        ;;
                      djcr)
-                       f90nml -g namdyn_hpg -v ln_hpg_djr=.true. -v ln_hpg_ref=.true. -v ln_hpg_ref_str=.true. -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
+                       f90nml -g namdyn_hpg \
+                              -v ln_hpg_djr=.true. \
+                              -v ln_hpg_ref=.true. \
+                              -v ln_hpg_ref_str=.true. \
+                              -v ln_hpg_ref_ccs=.true. \
+                              -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
                        ;;
                      ffl)
-                       f90nml -g namdyn_hpg -v ln_hpg_ffr=.true. -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
+                       f90nml -g namdyn_hpg \
+                              -v ln_hpg_ffr=.true. \
+                              -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
                        ;;
                      ffq_cub)
-                       f90nml -g namdyn_hpg -v ln_hpg_ffr=.true. -v ln_hpg_ffr_vrt_quad=.true. -v ln_hpg_ffr_hor_cub=.true. -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
+                       f90nml -g namdyn_hpg \
+                              -v ln_hpg_ffr=.true. \
+                              -v ln_hpg_ffr_vrt_quad=.true. \
+                              -v ln_hpg_ffr_hor_cub=.true. \
+                              -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
                        ;;
                      ffq_ccs)
-                       f90nml -g namdyn_hpg -v ln_hpg_ffr=.true. -v ln_hpg_ffr_vrt_quad=.true. -v ln_hpg_ffr_hor_ccs=.true. -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
+                       f90nml -g namdyn_hpg \
+                              -v ln_hpg_ffr=.true. \
+                              -v ln_hpg_ffr_vrt_quad=.true. \
+                              -v ln_hpg_ffr_hor_ccs=.true. \
+                              -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
                        ;;
                      fflr)
-                       f90nml -g namdyn_hpg -v ln_hpg_ffr=.true. -v ln_hpg_ref=.true. -v ln_hpg_ref_str=.true. -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
+                       f90nml -g namdyn_hpg \
+                              -v ln_hpg_ffr=.true. \
+                              -v ln_hpg_ref=.true. \
+                              -v ln_hpg_ref_str=.true. \
+                              -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
+                       ;;
+                     ffq_cubr)
+                       f90nml -g namdyn_hpg \
+                              -v ln_hpg_ffr=.true. \
+                              -v ln_hpg_ffr_vrt_quad=.true. \
+                              -v ln_hpg_ffr_hor_cub=.true. \
+                              -v ln_hpg_ref=.true. \
+                              -v ln_hpg_ref_str=.true. \
+                              -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3"
+                       ;;
+                     ffq_ccsr)
+                       f90nml -g namdyn_hpg \
+                              -v ln_hpg_ffr=.true. \
+                              -v ln_hpg_ffr_vrt_quad=.true. \
+                              -v ln_hpg_ffr_hor_ccs=.true. \
+                              -v ln_hpg_ref=.true. \
+                              -v ln_hpg_ref_str=.true. \
+                              -v ln_hpg_ref_ccs=.true. \
+                              -p ${nam_cfg}".tmp2" ${nam_cfg}".tmp3" 
                    esac
  
                    # Computational halo
-                   #if [ "${hpg}" == djr ] || [ "${hpg}" == ffq ]; then
-                      f90nml -g nammpp -v nn_hls=2 -p ${nam_cfg}".tmp3" ${nam_cfg}".tmp4"
-                   #else
-                   #   cp ${nam_cfg}".tmp3" ${nam_cfg}".tmp4"
-                   #fi
+                   f90nml -g nammpp \
+                          -v nn_hls=2 \
+                          -p ${nam_cfg}".tmp3" ${nam_cfg}".tmp4"
 
                    # 4. Initial condition formulation
                    case ${ini} in
                      pnt)
-                       f90nml -g namusr_def -v ln_init_pt_val=.true. -p ${nam_cfg}".tmp4" ${nam_cfg}".tmp5"
+                       f90nml -g namusr_def \
+                              -v ln_init_pt_val=.true. \
+                              -p ${nam_cfg}".tmp4" ${nam_cfg}".tmp5"
                        ;;
                      ave)
-                       f90nml -g namusr_def -v ln_init_pt_val=.false. -v ln_init_curved=.false. -p ${nam_cfg}".tmp4" ${nam_cfg}".tmp5"
+                       f90nml -g namusr_def \
+                              -v ln_init_pt_val=.false. \
+                              -v ln_init_curved=.false. \
+                              -p ${nam_cfg}".tmp4" ${nam_cfg}".tmp5"
                        ;;
                    esac
 
                    # 5. Coriolis parameter
                    case ${cor} in
                      fp4)
-                       f90nml -g namusr_def -v rn_fplane=1.0e-4 -p ${nam_cfg}".tmp5" ${nam_cfg}
+                       f90nml -g namusr_def \
+                              -v rn_fplane=1.0e-4 \
+                              -p ${nam_cfg}".tmp5" ${nam_cfg}
                        ;;
                      fp5)
-                       f90nml -g namusr_def -v rn_fplane=1.0e-5 -p ${nam_cfg}".tmp5" ${nam_cfg}
+                       f90nml -g namusr_def \
+                              -v rn_fplane=1.0e-5 \
+                              -p ${nam_cfg}".tmp5" ${nam_cfg}
                        ;;
                    esac
 
